f3f2c499b3b37e5cf969ea16926bac401ab4f69a
Shouryajajodia <87896065+Shouryajajodia@users.noreply.github.com>
2021-09-25 15:00:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
-    canvas=createCanvas(300,300)
+
+function setup() {
+    canvas = createCanvas(300, 300)
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/-UUst5dMA/model.json", modelLoaded);
+
+
 }
-function draw(){
-    
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotresult);
+}
+
+function modelLoaded() {
+    console.log("model loaded");
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+        document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
